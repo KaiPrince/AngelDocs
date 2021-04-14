ccb8ff09a92513e04e4c5b1050e56f1e7d2ed33f
KaiPrince <34746763+KaiPrince@users.noreply.github.com>
2021-04-14 00:17:25 +0000

    Deploying to gh-pages from @ KaiPrince/AngelDocs@86da5b49ae29afd6ae0cdd3229fb4a41e538caf1 🚀

---
+++ assets/projects_angel-docs_src_angel-docs_config.md.0ef3d8a0.lean.js
@@ -0,0 +1,1 @@
+import{o as n,c as a,a as s}from"./app.3fd7882d.js";const t='{"title":"","description":"","frontmatter":{},"relativePath":"projects/angel-docs/src/angel-docs/config.md","lastUpdated":1618359436695}',p={},o=s('',2);p.render=function(s,t,p,e,c,i){return n(),a("div",null,[o])};export default p;export{t as __pageData};
